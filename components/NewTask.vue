5adbb752f85a85b431560df659a0db0b052e74c4
Bilel Barhoumi <infobilel@gmail.com>
2023-11-05 22:05:50 +0100

    First commeit

---
+++ components/NewTask.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type {Task} from "~/types";
+import {nanoid} from "nanoid";
+
+const emit = defineEmits<{
+  (e: "add", payload: Task): void
+}>()
+
+const focused = ref(false);
+const title = ref("");
+
+function createTask(e: Event) {
+  if (title.value.trim()) {
+    e.preventDefault();
+    emit("add", {
+      title: title.value.trim(),
+      createdAt: new Date(),
+      id: nanoid(),
+    } as Task)
+  }
+
+  title.value = "";
+}
+</script>
+
+<template>
+  <div>
+    <textarea v-model="title"
+              @keydown.tab="createTask"
+              @keydown.enter="createTask"
+              class="focus:bg-white focus:shadow bg-transparent resize-none rounded w-full border-none focus:p-2"
+              :class="{
+                'h-7': !focused,
+                'h-20': focused,
+              }"
+              style="outline: none!important;"
+              @focus="focused = true"
+              @blur="focused = false"
+              :placeholder="!focused ? '+ Add new Card' : 'Enter title for this Card'"
+
+    />
+  </div>
+</template>
---
+++ components/TrelloBoardTask.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type {Task} from "~/types";
+
+defineProps<{
+  task: Task
+}>()
+</script>
+
+<template>
+  <div :title="task.createdAt.toLocaleString()"
+       class="task bg-white p-2 mb-2 rounded max-w-[520px] flex">
+    <Icon class="drag-handle cursor-move min-w-[15px]" name="material-symbols:table-rows-narrow-outline"/>
+
+    {{ task.title }}
+  </div>
+</template>
+
+<style scoped>
+.sortable-drag .task {
+  transform: rotate(5deg);
+}
+
+.sortable-ghost .task {
+  position: relative;
+}
+
+.sortable-ghost .task:after {
+  content: "";
+  @apply absolute top-0 bottom-0 left-0 right-0 bg-slate-300 rounded;
+}
+</style>
